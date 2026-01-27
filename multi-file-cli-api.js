7551f48689432622b043a1c62fbfc06e3e5f7832
Pellaeon Lin <nfsmwlin@gmail.com>
2026-01-27 12:36:41 +0000

    ADD: lay the groundwork for new multifile tool

---
+++ multi-file-cli-api.js
@@ -0,0 +1,373 @@
+/*
+ * Copyright 2010-2024 Gildas Lormeau
+ * contact : gildas.lormeau <at> gmail.com
+ *
+ * This file is part of SingleFile.
+ *
+ *   The code in this file is free software: you can redistribute it and/or
+ *   modify it under the terms of the GNU Affero General Public License
+ *   (GNU AGPL) as published by the Free Software Foundation, either version 3
+ *   of the License, or (at your option) any later version.
+ *
+ *   The code in this file is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ *   General Public License for more details.
+ *
+ *   As additional permission under GNU AGPL version 3 section 7, you may
+ *   distribute UNMODIFIED VERSIONS OF THIS file without the copy of the GNU
+ *   AGPL normally required by section 4, provided you include this license
+ *   notice and a URL through which recipients can access the Corresponding
+ *   Source.
+ */
+
+/* global URL, Blob, FileReader */
+
+import * as backend from "./lib/cdp-client.js";
+import { getZipScriptSource } from "./lib/multi-file-script.js";
+import { Deno, path } from "./lib/deno-polyfill.js";
+
+const VALID_URL_TEST = /^(https?|file):\/\//;
+
+const DEFAULT_OPTIONS = {
+	removeHiddenElements: true,
+	removeUnusedStyles: true,
+	removeUnusedFonts: true,
+	compressHTML: true,
+	loadDeferredImages: true,
+	loadDeferredImagesMaxIdleTime: 1500,
+	filenameTemplate: "%if-empty<{page-title}|No title> ({date-locale} {time-locale}).{filename-extension}",
+	filenameMaxLength: 192,
+	filenameMaxLengthUnit: "bytes",
+	filenameReplacedCharacters: ["~", "+", "?", "%", "*", ":", "|", "\"", "<", ">", "\\\\", "\x00-\x1f", "\x7F"],
+	filenameReplacementCharacter: "_",
+	filenameReplacementCharacters: ["～", "＋", "？", "％", "＊", "：", "｜", "＂", "＜", "＞", "＼"],
+	maxResourceSize: 10,
+	backgroundSave: true,
+	removeAlternativeFonts: true,
+	removeAlternativeMedias: true,
+	removeAlternativeImages: true,
+	groupDuplicateImages: true,
+	saveFavicon: true,
+	insertMetaCSP: true,
+	insertSingleFileComment: true,
+	blockScripts: true,
+	blockVideos: true,
+	blockAudios: true
+};
+const STATE_PROCESSING = "processing";
+const STATE_PROCESSED = "processed";
+
+const { readTextFile, writeTextFile, writeFile, stdout, mkdir, stat, errors } = Deno;
+let tasks = [], maxParallelWorkers, sessionFilename;
+
+export { initialize };
+
+async function initialize(options) {
+	options = Object.assign({}, DEFAULT_OPTIONS, options);
+	maxParallelWorkers = options.maxParallelWorkers || 8;
+	try {
+		await backend.initialize(options);
+	} catch (error) {
+		if (error instanceof errors.NotFound) {
+			let message = "Chromium executable not found. ";
+			if (options.browserExecutablePath) {
+				message += "Make sure --browser-executable-path is correct.";
+			} else {
+				message += "Set the path using the --browser-executable-path option.";
+			}
+			throw new Error(message);
+		} else {
+			throw error;
+		}
+	}
+	if (options.crawlSyncSession || options.crawlLoadSession) {
+		try {
+			tasks = JSON.parse(await readTextFile(options.crawlSyncSession || options.crawlLoadSession));
+		} catch (error) {
+			if (options.crawlLoadSession) {
+				throw error;
+			}
+		}
+	}
+	if (options.crawlSyncSession || options.crawlSaveSession) {
+		sessionFilename = options.crawlSyncSession || options.crawlSaveSession;
+	}
+	return {
+		capture: urls => capture(urls, options),
+		finish: () => finish(options)
+	};
+}
+
+async function capture(urls, options) {
+	let newTasks;
+	const taskUrls = tasks.map(task => task.url);
+	newTasks = await Promise.all(urls.map(value => {
+		let url, taskOptions;
+		if (Array.isArray(value)) {
+			url = value[0];
+			taskOptions = Object.assign({}, options, value[1]);
+		} else {
+			url = value;
+			taskOptions = options;
+		}
+		return createTask(url, taskOptions);
+	}));
+	newTasks = newTasks.filter(task => task && !taskUrls.includes(task.url));
+	if (newTasks.length) {
+		tasks = tasks.concat(newTasks);
+		await saveTasks();
+	}
+	await runTasks();
+}
+
+async function finish(options) {
+	const promiseTasks = tasks.map(task => task.promise);
+	await Promise.all(promiseTasks);
+	if (options.crawlReplaceURLs && !options.compressContent) {
+		for (const task of tasks) {
+			try {
+				let pageContent = await readTextFile(task.filename);
+				tasks.forEach(otherTask => {
+					if (otherTask.filename) {
+						pageContent = pageContent.replace(new RegExp(escapeRegExp("\"" + otherTask.originalUrl + "\""), "gi"), "\"" + otherTask.filename + "\"");
+						pageContent = pageContent.replace(new RegExp(escapeRegExp("'" + otherTask.originalUrl + "'"), "gi"), "'" + otherTask.filename + "'");
+						const filename = otherTask.filename.replace(/ /g, "%20");
+						pageContent = pageContent.replace(new RegExp(escapeRegExp("=" + otherTask.originalUrl + " "), "gi"), "=" + filename + " ");
+						pageContent = pageContent.replace(new RegExp(escapeRegExp("=" + otherTask.originalUrl + ">"), "gi"), "=" + filename + ">");
+					}
+				});
+				await writeTextFile(task.filename, pageContent);
+			} catch {
+				// ignored
+			}
+		}
+	}
+	if (!options.browserDebug && !options.browserServer) {
+		return backend.closeBrowser();
+	}
+}
+
+function runTasks() {
+	const availableTasks = tasks.filter(task => !task.status).length;
+	const processingTasks = tasks.filter(task => task.status == STATE_PROCESSING).length;
+	const promisesTasks = [];
+	for (let workerIndex = 0; workerIndex < Math.min(availableTasks, maxParallelWorkers - processingTasks); workerIndex++) {
+		promisesTasks.push(runNextTask());
+	}
+	return Promise.all(promisesTasks);
+}
+
+async function runNextTask() {
+	const task = tasks.find(task => !task.status);
+	if (task) {
+		const options = task.options;
+		const taskOptions = JSON.parse(JSON.stringify(options));
+		taskOptions.url = task.url;
+		task.status = STATE_PROCESSING;
+		await saveTasks();
+		task.promise = capturePage(taskOptions);
+		const pageData = await task.promise;
+		task.status = STATE_PROCESSED;
+		if (pageData) {
+			task.filename = pageData.filename;
+			if (options.crawlLinks && testMaxDepth(task)) {
+				const urls = pageData.links;
+				let newTasks = await Promise.all(urls.map(url => createTask(url, options, task, task.rootTaskURL || task.url)));
+				newTasks = newTasks.filter(task => task &&
+					testMaxDepth(task) &&
+					!tasks.find(otherTask => otherTask.url == task.url) &&
+					!newTasks.find(otherTask => otherTask != task && otherTask.url == task.url) &&
+					(!options.crawlInnerLinksOnly || task.isInnerLink) &&
+					(!options.crawlNoParent || (task.isChild || !task.isInnerLink)));
+				tasks.splice(tasks.length, 0, ...newTasks);
+			}
+		}
+		await saveTasks();
+		await runTasks();
+	}
+}
+
+function testMaxDepth(task) {
+	const options = task.options;
+	return (options.crawlMaxDepth == 0 || task.depth <= options.crawlMaxDepth) &&
+		(options.crawlExternalLinksMaxDepth == 0 || task.externalLinkDepth < options.crawlExternalLinksMaxDepth);
+}
+
+async function createTask(url, options, parentTask, rootTaskURL) {
+	options.originalUrl = url;
+	url = parentTask ? rewriteURL(url, options.crawlRemoveURLFragment, options.crawlRewriteRules) : url;
+	if (url) {
+		if (!VALID_URL_TEST.test(url)) {
+			try {
+				url = url.replace(/\\/g, "/");
+				url = url.replace(/#/g, "%23");
+				const baseURL = await path.toFileUrl((await Deno.cwd()) + path.SEPARATOR);
+				url = new URL(url, baseURL).href;
+			} catch (error) {
+				throw new Error("Invalid URL or file path: " + url, { cause: error });
+			}
+		}
+		const isInnerLink = rootTaskURL && url.startsWith(getHostURL(rootTaskURL));
+		const rootBaseURIMatch = rootTaskURL && rootTaskURL.match(/(.*?)[^/]*$/);
+		const isChild = isInnerLink && rootBaseURIMatch && rootBaseURIMatch[1] && url.startsWith(rootBaseURIMatch[1]);
+		return {
+			url,
+			isInnerLink,
+			isChild,
+			originalUrl: url,
+			rootTaskURL,
+			depth: parentTask ? parentTask.depth + 1 : 0,
+			externalLinkDepth: isInnerLink ? -1 : parentTask ? parentTask.externalLinkDepth + 1 : -1,
+			options
+		};
+	}
+}
+
+async function saveTasks() {
+	if (sessionFilename) {
+		await writeTextFile(sessionFilename, JSON.stringify(
+			tasks.map(task => Object.assign({}, task, {
+				status: task.status == STATE_PROCESSING ? undefined : task.status,
+				promise: undefined,
+				options: task.status && task.status == STATE_PROCESSED ? undefined : task.options
+			}))
+		));
+	}
+}
+
+function rewriteURL(url, crawlRemoveURLFragment, crawlRewriteRules = []) {
+	url = url.trim();
+	if (crawlRemoveURLFragment) {
+		url = url.replace(/^(.*?)#.*$/, "$1");
+	}
+	crawlRewriteRules.forEach(rewriteRule => {
+		const parts = rewriteRule.trim().split(/ +/);
+		if (parts.length) {
+			url = url.replace(new RegExp(parts[0]), parts[1] || "").trim();
+		}
+	});
+	return url;
+}
+
+function getHostURL(url) {
+	url = new URL(url);
+	return url.protocol + "//" + (url.username ? url.username + (url.password || "") + "@" : "") + url.hostname;
+}
+
+async function capturePage(options) {
+	try {
+		let filename, content;
+		options.zipScript = getZipScriptSource();
+		const pageData = await backend.getPageData(options);
+		console.debug(`pagedata: `, Object.keys(pageData));
+		content = pageData.content;
+		if (options.consoleMessagesFile && pageData.consoleMessages) {
+			await writeTextFile(options.consoleMessagesFile, JSON.stringify(pageData.consoleMessages, null, 2));
+		}
+		if (options.debugMessagesFile && pageData.debugMessages) {
+			await writeTextFile(options.debugMessagesFile, pageData.debugMessages.map(([timestamp, message]) =>
+				`[${new Date(timestamp).toISOString()}] ${message.join(" ")}`).join("\n"));
+		}
+		if (options.outputJson) {
+			if (content instanceof Uint8Array) {
+				const fileReader = new FileReader();
+				fileReader.readAsDataURL(new Blob([content]));
+				content = await new Promise(resolve => {
+					fileReader.onload = () => resolve(fileReader.result);
+				});
+				content = content.replace(/^data:.*?;base64,/, "");
+				pageData.content = undefined;
+				pageData.binaryContent = content;
+			}
+			pageData.doctype = undefined;
+			pageData.viewport = undefined;
+			pageData.comment = undefined;
+			content = JSON.stringify(pageData, null, 2);
+		}
+		if (options.output) {
+			filename = await getFilename(options.output, options);
+		} else if (options.dumpContent) {
+			if (options.compressContent) {
+				await stdout.write(content);
+			} else {
+				console.log(content || ""); // eslint-disable-line no-console
+			}
+		} else {
+			filename = await getFilename(pageData.filename, options);
+		}
+		if (filename) {
+			if (options.outputJson) {
+				filename += filename.endsWith(".json") ? "" : ".json";
+			}
+			const directoryName = path.dirname(filename);
+			if (directoryName !== ".") {
+				await mkdir(directoryName, { recursive: true });
+			}
+			if (content instanceof Uint8Array) {
+				await writeFile(filename, content);
+			} else {
+				await writeTextFile(filename, content);
+			}
+		}
+		return pageData;
+	} catch (error) {
+		const date = new Date();
+		let message = `[${date.toISOString()}] URL: ${options.url}`;
+		if (!options.errorsTracesDisabled) {
+			message += "\nStack: " + error.stack;
+		}
+		message += "\n";
+		if (options.errorsFile) {
+			await writeTextFile(options.errorsFile, message, { append: true });
+		} else {
+			console.error(error.message || error, message); // eslint-disable-line no-console
+		}
+		if (options.consoleMessagesFile && error.consoleMessages) {
+			await writeTextFile(options.consoleMessagesFile, JSON.stringify(error.consoleMessages, null, 2));
+		}
+		if (options.debugMessagesFile && error.debugMessages) {
+			await writeTextFile(options.debugMessagesFile, error.debugMessages.map(([timestamp, message]) =>
+				`[${new Date(timestamp).toISOString()}] ${message.join(" ")}`).join("\n"));
+		}
+	}
+}
+
+async function getFilename(filename, options, index = 1) {
+	if (Array.isArray(options.outputDirectory)) {
+		const outputDirectory = options.outputDirectory.pop();
+		if (outputDirectory.startsWith("/")) {
+			options.outputDirectory = outputDirectory;
+		} else {
+			options.outputDirectory = options.outputDirectory[0] + outputDirectory;
+		}
+	}
+	let outputDirectory = options.outputDirectory || "";
+	if (outputDirectory && !outputDirectory.endsWith("/")) {
+		outputDirectory += "/";
+	}
+	let newFilename = outputDirectory + filename;
+	if (options.filenameConflictAction == "overwrite") {
+		return filename;
+	} else if (options.filenameConflictAction == "uniquify" && index > 1) {
+		const regExpMatchExtension = /(\.[^.]+)$/;
+		const matchExtension = newFilename.match(regExpMatchExtension);
+		if (matchExtension && matchExtension[1]) {
+			newFilename = newFilename.replace(regExpMatchExtension, " (" + index + ")" + matchExtension[1]);
+		} else {
+			newFilename += " (" + index + ")";
+		}
+	}
+	try {
+		await stat(newFilename);
+		if (options.filenameConflictAction != "skip") {
+			return getFilename(filename, options, index + 1);
+		}
+	} catch {
+		return newFilename;
+	}
+}
+
+function escapeRegExp(string) {
+	return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
---
+++ multi-file-launcher.js
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2010-2024 Gildas Lormeau
+ * contact : gildas.lormeau <at> gmail.com
+ *
+ * This file is part of SingleFile.
+ *
+ *   The code in this file is free software: you can redistribute it and/or
+ *   modify it under the terms of the GNU Affero General Public License
+ *   (GNU AGPL) as published by the Free Software Foundation, either version 3
+ *   of the License, or (at your option) any later version.
+ *
+ *   The code in this file is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ *   General Public License for more details.
+ *
+ *   As additional permission under GNU AGPL version 3 section 7, you may
+ *   distribute UNMODIFIED VERSIONS OF THIS file without the copy of the GNU
+ *   AGPL normally required by section 4, provided you include this license
+ *   notice and a URL through which recipients can access the Corresponding
+ *   Source.
+ */
+
+import { initialize } from "./multi-file-cli-api.js";
+import { closeBrowser } from "./lib/browser.js";
+import { Deno } from "./lib/deno-polyfill.js";
+import { options, parseArgs } from "./options.js";
+
+const { readTextFile, readFile, exit, addSignalListener } = Deno;
+
+try {
+	addSignalListener("SIGTERM", closeBrowserAndExit);
+} catch (_error) {
+	// ignored
+}
+try {
+	addSignalListener("SIGINT", closeBrowserAndExit);
+} catch (_error) {
+	// ignored
+}
+
+export { run };
+
+async function run() {
+	try {
+		let urls;
+		if (options.settingsFile) {
+			const settings = JSON.parse(await Deno.readTextFile(options.settingsFile));
+			let profileName = options.settingsProfile || "default";
+			if (profileName == "default" || !settings.profiles[profileName]) {
+				profileName = "__Default_Settings__";
+			}
+			Object.assign(options, settings.profiles[profileName]);
+			delete options.settingsFile;
+		}
+		if (options.urlsFile) {
+			urls = await getUrlsFile(options.urlsFile);
+		} else {
+			urls = [options.url];
+		}
+		if (options.browserCookies) {
+			const cookies = [];
+			for (const cookie of options.browserCookies) {
+				const [name, value, domain, path, expires, httpOnly, secure, sameSite, url] = cookie.split(",");
+				cookies.push({
+					name,
+					value,
+					url,
+					domain,
+					path,
+					secure: secure === "true",
+					httpOnly: httpOnly === "true",
+					sameSite,
+					expires: isNaN(Number(expires)) ? undefined : Number(expires)
+				});
+			}
+			options.browserCookies = cookies;
+		}
+		if (options.browserCookiesFile) {
+			const cookiesContent = await readTextFile(options.browserCookiesFile);
+			try {
+				options.browserCookies = JSON.parse(cookiesContent);
+			} catch {
+				options.browserCookies = parseCookies(cookiesContent);
+			}
+		}
+		if (options.httpHeaders) {
+			const headers = {};
+			for (const header of options.httpHeaders) {
+				const [name, value] = header.split("=");
+				headers[name] = value.trim();
+			}
+			options.httpHeaders = headers;
+		}
+		if (options.embeddedImage) {
+			options.embeddedImage = Array.from(await readFile(options.embeddedImage));
+		}
+		if (options.embeddedPdf) {
+			options.embeddedPdf = Array.from(await readFile(options.embeddedPdf));
+		}
+		options.retrieveLinks = true;
+		const singlefile = await initialize(options);
+		await singlefile.capture(urls);
+		await singlefile.finish();
+	} catch (error) {
+		console.error(error.message || error); // eslint-disable-line no-console
+		await closeBrowserAndExit(-1);
+	}
+}
+
+function parseCookies(textValue) {
+	const httpOnlyRegExp = /^#HttpOnly_(.*)/;
+	return textValue.split(/\r\n|\n/)
+		.filter(line => line.trim() && (!/^#/.test(line) || httpOnlyRegExp.test(line)))
+		.map(line => {
+			const httpOnly = httpOnlyRegExp.test(line);
+			if (httpOnly) {
+				line = line.replace(httpOnlyRegExp, "$1");
+			}
+			const values = line.split(/\t/);
+			if (values.length == 7) {
+				return {
+					domain: values[0],
+					path: values[2],
+					secure: values[3] == "TRUE",
+					expires: (values[4] && Number(values[4])) || undefined,
+					name: values[5],
+					value: values[6],
+					httpOnly
+				};
+			}
+		})
+		.filter(cookieData => cookieData);
+}
+
+async function closeBrowserAndExit(code) {
+	await closeBrowser();
+	exit(code);
+}
+
+async function getUrlsFile(urlsFile) {
+	let urls = (await readTextFile(urlsFile)).split("\n");
+	urls = urls.map(value => {
+		value = value.trim();
+		let optionPosition = value.indexOf(" --");
+		if (optionPosition < 0) {
+			optionPosition = value.indexOf("\t--");
+		}
+		if (optionPosition > 0) {
+			const url = value.substring(0, optionPosition).trim();
+			const argsString = value.substring(optionPosition + 1).trim();
+			const args = [];
+			let previousCharacter, previousPreviousCharacter, lastQuoteCharacter;
+			let lastCharIndex = 0;
+			for (let currentCharIndex = 0; currentCharIndex < argsString.length; currentCharIndex++) {
+				const character = argsString[currentCharIndex];
+				if (character == lastQuoteCharacter && (previousCharacter != "\\" || previousPreviousCharacter == "\\")) {
+					args.push(argsString.substring(lastCharIndex, currentCharIndex));
+					lastQuoteCharacter = null;
+					lastCharIndex = currentCharIndex + 1;
+				} else if (!lastQuoteCharacter) {
+					if (character == "'" || character == "\"") {
+						lastQuoteCharacter = argsString[currentCharIndex];
+						lastCharIndex = currentCharIndex + 1;
+					} else {
+						const isSpaceCharacter = character == " " || character == "\t";
+						if (isSpaceCharacter || character == "=") {
+							if (isSpaceCharacter && (currentCharIndex == lastCharIndex + 1)) {
+								lastCharIndex++;
+							} else if (lastCharIndex < currentCharIndex) {
+								args.push(argsString.substring(lastCharIndex, currentCharIndex));
+								lastCharIndex = currentCharIndex + 1;
+							} else {
+								lastCharIndex = currentCharIndex + 1;
+							}
+						}
+					}
+				}
+				previousPreviousCharacter = previousCharacter;
+				previousCharacter = character;
+			}
+			if (lastCharIndex < argsString.length) {
+				args.push(argsString.substring(lastCharIndex).trim());
+			}
+			const { options } = parseArgs(args, false);
+			return [url, options];
+		} else {
+			return value;
+		}
+	});
+	return urls;
+}
